0e234232b679fa2ab402ba5dd5a232c293b6d40a
Seung Wook Kim <CEO@Rloha.io>
2018-10-31 17:54:49 +0900

    Update data_handling_day3_02.R

--- data_handling_day3_02.R
+++ data_handling_day3_02.R
@@ -1,3 +1,4 @@
+
 #### Q ####
 #### 1. 결측치 확인 ####
 # 각 column의 결측치를 확인하시오.
@@ -67,3 +68,37 @@ df$site_name %>%
 
 #### ____ (2) user_location_country ####
 #### ____ (3) hotel_country ####
+
+
+#### 3. 이변수 탐색 ####
+head(df)
+
+#### __ 1) 예약 x 모바일 ####
+# 모바일 접속자의 예약 비율과
+# 모바일로 접속하지 않은 사람의 계산하시오.
+df_m_0 = df[df$is_mobile == 0,] 
+df_m_1 = df[df$is_mobile == 1,] 
+
+sum(df_m_0$is_booking == 1) / nrow(df_m_0)
+sum(df_m_1$is_booking == 1) / nrow(df_m_1)
+
+# 모바일로 접속한 사람과 그렇지 않은 사람의 예약 비율을 검정하시오.
+prop.test(c(sum(df_m_0$is_booking == 1), sum(df_m_1$is_booking == 1)),
+          c(nrow(df_m_0), nrow(df_m_1)))
+
+
+#### __ 2) 예약 x 국가 ####
+#### ____ (1) 변수 생성 ####
+# 사용자의 국가와 여행하는 국가의 일치 여부를 확인하는
+# 변수를 새로 생성하시오.
+df[, "domestic"] = ifelse(df$user_location_country == df$hotel_country,
+                          yes = 1, no = 0)
+
+#### ____ (2) 확인 ####
+# (사용자 기준) 국내 여행을 검색한 사람의 예약비율과
+#               해외 여행을 검색한 사람의 예약비율을 구하시오.
+df_d_1 = df[df$domestic == 1, ]
+df_d_0 = df[df$domestic == 0, ]
+
+sum(df_d_1$is_booking == 1) / nrow(df_d_1)
+sum(df_d_0$is_booking == 1) / nrow(df_d_0)
